f30ec64e9fab37ed3eb1f4d5dcb08217d0bc417b
fedora Cloud User <fedora@fink-dev.lal.in2p3.fr>
2024-06-03 11:54:39 +0000

    MLflow project

---
+++ image_classification/train.py
@@ -0,0 +1,32 @@
+import argparse
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+def train(epochs, batch_size):
+    (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+    train_images, test_images = train_images / 255.0, test_images / 255.0
+
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, 
+              validation_data=(test_images, test_labels))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
+    train(args.epochs, args.batch_size)
